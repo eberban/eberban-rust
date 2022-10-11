dd40172f940bfecc5b2b2ec74025c738582cdb75
mia entropy <63742169+mia-entropy@users.noreply.github.com>
2022-10-11 19:24:14 +0200

    choice now supports Vec + rollback wrap

--- crates/framework/src/parser/choice.rs
+++ crates/framework/src/parser/choice.rs
@@ -1,13 +1,13 @@
-use crate::{OptionInOk, ParseResult, Parser, Stream};
+use crate::{OptionInErr, OptionInOk, ParseResult, Parser, Stream};
 
-/// Parse using a tuple of parsers having the same input and output types.
+/// Parse using a `Vec` or tuple of parsers having the same input and output types.
 ///
 /// Will try each one in order, will return an error as soon as one errors, and
 /// return the output of the first one that parses. If none parses, this parser
 /// don't parse too.
 ///
 /// Equivalent of chaining many parsers using [`or`](super::OrExt::or).
-/// 
+///
 /// It is implemented for tuples containing up to 40 members, which should be enough
 /// for must usages. If more are needed, use another second choice inside the
 /// first one or use [`or`](super::OrExt::or).
@@ -18,6 +18,37 @@ pub fn choice<T>(tuple: T) -> Choice<T> {
 /// See [`choice`].
 #[derive(Debug, Clone, Copy)]
 pub struct Choice<Tuple>(Tuple);
+
+impl<S, I, O, E, A> Parser<S, I, O> for Choice<Vec<A>>
+where
+    A: Parser<S, I, O, Error = E>,
+{
+    type Error = E;
+
+    fn parse(&self, stream: &mut S) -> ParseResult<O, Self::Error>
+    where
+        S: Stream<I, Self::Error>,
+    {
+        stream
+            .transaction(|stream| {
+                // We try each parser in order.
+                for member in &self.0 {
+                    let out = stream
+                        .transaction(|stream| member.parse(stream).option_in_err())
+                        .option_in_ok()?;
+
+                    // If it parsed then we return the result.
+                    if let Some(out) = out {
+                        return Ok(out);
+                    }
+                }
+
+                // None parsed.
+                Err(None)
+            })
+            .option_in_ok()
+    }
+}
 
 macro_rules! impl_for_tuple {
     () => {};
@@ -47,7 +78,9 @@ macro_rules! impl_for_tuple {
 
                         // We try each parser in order.
                         $(
-                            let out = $member.parse(stream)?;
+                            let out = stream.transaction(|stream| {
+                                $member.parse(stream).option_in_err()
+                            }).option_in_ok()?;
 
                             // If it parsed then we return the result.
                             if let Some(out) = out {
